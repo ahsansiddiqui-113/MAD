6d3566e5bc03ea37afabca7cd3317f7b44c3777c
ahsansiddiqui-113 <100269036+ahsansiddiqui-113@users.noreply.github.com>
2022-02-23 12:35:03 +0500

    MAD TASKS

---
+++ ahsan.js
@@ -0,0 +1,94 @@
+
+// Problem_01------------------------------------------------
+// let num=23617
+// let k=4
+// while(k>1){
+//     num=parseInt(num/10);
+//     k=k-1;
+//     console.log(num);
+// }
+// console.log("The Result is:",num%10);
+
+// Problem_02--------------------------------------------
+// const numbers = [2,3,6,1,7]
+// const sum = even.reduce((total, number, index, array) => {
+//   return total + number
+// })
+// console.log(sum)
+
+// Problem_03----------------------------------------------
+// const numbers = [2,3,6,1,7]
+// const even = numbers.filter((num)=>num%2==0)
+// console.log(even)
+// const sum = even.reduce((total, number, index, array) => {
+//   return total + number
+// })
+// console.log(sum)
+
+// Problem_04------------------------------------------------
+// let start =0;
+// while(start<10){
+//     console.log("---------")
+//     console.log("Cube of",start,start**3);
+//     console.log("Squre of ",start,start);
+//     start=start+1;
+// }
+
+//Problem_05----------------------------------------------
+// let number=[1,2,3,45,6,88]
+// console.log("Before",number)
+//  const revnumber=number.reverse();
+//  console.log("reverse",revnumber);
+//  let tostr= revnumber.toString();
+//  console.log("ToString",tostr)
+// //05__Part-------------
+// //A, E, I, O, U
+// let str="COMSATS
+// var tolower=str.toLowerCase();
+// console.log(tolower)
+// for(i=0;i<=str.length;i++){
+//     if(str[i]=="a")
+//     {
+//         console.log([i])
+//         break
+//     }
+//     if(str[i]=="e"){
+//         console.log([i])
+//         break
+//     }
+//     if(str[i]=="i"){
+//         console.log([i])
+//         break
+//     }
+//     if(str[i]=="o"){
+//         console.log([i])
+//         break
+//     }
+//     if (str[i]=="u"){
+//         console.log([i])
+//         break
+//     }
+
+// }
+
+
+// Problem_06------------------------------------------------
+// let num =Math.ceil(Math.random()*10);
+// let guess_number=6
+// if (guess_number==num){
+//     console.log('Matched & You Win!');
+// }
+
+//Problem_07----------------------------------------------
+// let array=[2,3,4,10,9,5,10]
+// for(i=0;i<=array.length;i++)
+// {
+//     if (array[i]==10)
+//     {
+//         console.log("10 appears in first or last position ",[i])
+//     }
+// }
+
+//Problem_08-------------------------------------------------
+// let string="COMSATS"
+// console.log(string.split("").sort().join(""))
